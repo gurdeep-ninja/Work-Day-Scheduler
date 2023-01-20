4ac9683358bf53d3b590f3348010bc0de15b03ca
Gurdeep Bangar <gurdeepbangar@protonmail.com>
2023-01-20 20:24:06 +0000

    Removed AM/PM string calculation, and added time format in schedule object key instead

--- js/schedule.js
+++ js/schedule.js
@@ -1,39 +1,48 @@
 // An array of objects storing the schedule tasks
 let schedule = [
     {
-        hour:9,
-        content:""
+        hour: 9,
+        hour12:"9am",
+        content: ""
     },
     {
-        hour:10,
-        content:""
+        hour: 10,
+        hour12:"10am",
+        content: ""
     },
     {
-        hour:11,
-        content:""
+        hour: 11,
+        hour12:"11am",
+        content: ""
     },
     {
-        hour:12,
-        content:""
+        hour: 12,
+        hour12:"12pm",
+        content: ""
     },
     {
-        hour:13,
-        content:""
+        hour: 13,
+        hour12:"1pm",
+        content: ""
     },
     {
-        hour:14,
-        content:""
-    }, 
+        hour: 14,
+        hour12:"2pm",
+        content: ""
+    },
     {
-        hour:15,
-        content:""
-    },  
+        hour: 15,
+        hour12:"3pm",        
+        content: ""
+    },
     {
-        hour:16,
-        content:""
-    },  
+        hour: 16,
+        hour12:"4pm",        
+        content: ""
+    },
     {
-        hour:17,
-        content:""
-    },                           
+        hour: 17,
+        hour12:"5pm",
+        content: ""
+    },
 ]
--- js/script.js
+++ js/script.js
@@ -8,31 +8,31 @@ schedule.forEach(function(element){
     //Create a html row using bootstrap class & ass data attribute called hour
     let row = $(`<div class="row" data-hour=${element.hour}>`)
 
-    // div for time
+    // column for time
     let divTime = $("<div>").addClass("col-2").addClass("my-auto")
 
-    // Figure out if schedule time is am or pm
-    let ampm
+    // append ampm to the element.hour so it appears like "12pm etc"
+    divTime.text(element.hour12)
 
-    if(element.hour < 12){
-        ampm = 'am'
-
-    } else if (element.hour >= 12 ){
-        ampm = 'pm'
-    }
-
-
-    divTime.text(element.hour + ampm)
-
-    // div for text area
+    // column for text area
     let divText= $("<div>").addClass("col-8").addClass("my-auto")    
 
-    // div for text save
+    // column for save button
     let divSave= $("<div>").addClass("col-2").addClass("my-auto")
+
+    // Create save button
+    let saveButton = "<button>Save</button>"
     
+    // Append the saveButton to divSave
+    divSave.append(saveButton)
+
+    // append div* elements to the row
+
     row.append(divTime)
     row.append(divText)
-    row.append(divSave)        
+    row.append(divSave)
+    
+    // Append the row to scheduleEl (".container")
     scheduleEl.append(row)
     console.log(divTime)
 })
