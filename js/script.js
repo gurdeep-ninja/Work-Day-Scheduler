ed06e42b9710e07eba3acebc118c5024d71e59f4
Gurdeep Bangar <gurdeepbangar@protonmail.com>
2023-01-20 21:18:47 +0000

    added an event listener for the saveBtn click to grab value from textarea element

--- js/script.js
+++ js/script.js
@@ -6,10 +6,10 @@ let scheduleEl = $(".container")
 let elCurrentDay = $('#currentDay')
 
 // Get & store the current date using moment.js
-var currentDay = moment();
+var currentDay = moment()
 
 // change the text of elCurrentDay to the value of currentDay and format it like "Thursday, Jan 5th ""
-elCurrentDay.text(currentDay.format("dddd, MMM Do"));
+elCurrentDay.text(currentDay.format("dddd, MMM Do"))
 
 
 // Loop through each item in the schedule array & create the html content
@@ -52,25 +52,41 @@ schedule.forEach(function(element){
     
     // Append the row to scheduleEl (".container")
     scheduleEl.append(row)
-    
-    
 })
 
-
+// A function to format the textArea element according to the current hour
 function formatColour(divText, hour){
 
-
-    console.log(divText)
+    //console.log(divText)
     // Get the current Hour
     var thisHour = parseInt(currentDay.format("H"))
 
     //console.log(`Hour: ${hour} thisHour: ${thisHour}`)
 
+    // if the task has expired add class task-expired
     if (hour < thisHour) {
         $(divText).find("textarea").addClass("task-expired")
+        // if its the current task add class task-current
     } else if (hour == thisHour) {
         $(divText).find("textarea").addClass("task-current")
+        // if its the next task add class task-next
     } else {
         $(divText).find("textarea").addClass("task-next")
     }
 }
+
+
+// Add an event listener to the save buttons (.saveBtn)
+$(".row").on("click",'.saveBtn',function(){
+
+    // Find out task's save button was clicked (stored as data attribute 'data-hour')
+    let scheduleTask = $(event.target).parent().parent().attr("data-hour")
+
+    // get the value of the textarea in the same row as the save button (.saveBtn)
+
+    //let scheduleTaskText = $(event.target).parent().parent().children().eq(1).children(0).val()
+    let scheduleTaskText = $(event.target).parent().prev().find("textarea").val()
+
+    console.log(`task: ${scheduleTask} text: ${scheduleTaskText}`)
+
+})
