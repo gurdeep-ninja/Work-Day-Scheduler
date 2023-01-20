86e4f68b5cd5f643c00be17cb40ea46d29bbfb68
Gurdeep Bangar <gurdeepbangar@protonmail.com>
2023-01-20 20:54:15 +0000

    added a function to add classes to element & updated stylesheet

--- js/script.js
+++ js/script.js
@@ -1,21 +1,25 @@
 
+// Select the HTML element .container
 let scheduleEl = $(".container")
 
 // Select the HTML element #currentDay
 let elCurrentDay = $('#currentDay')
-// Get the current date and format it like "Thursday, Jan 5th ""
-var currentDay = moment().format("dddd, MMM Do");
 
-// change the text of elCurrentDay to the value of currentDay
-elCurrentDay.text(currentDay);
+// Get & store the current date using moment.js
+var currentDay = moment();
+
+// change the text of elCurrentDay to the value of currentDay and format it like "Thursday, Jan 5th ""
+elCurrentDay.text(currentDay.format("dddd, MMM Do"));
 
 
 // Loop through each item in the schedule array & create the html content
 schedule.forEach(function(element){
-    //console.log(element.hour)
+
+    // get the hour from the schedule task
+    var hour = element.hour
 
     //Create a html row using bootstrap class & ass data attribute called hour
-    let row = $(`<div class="row" data-hour=${element.hour}>`)
+    let row = $(`<div class="row" data-hour=${hour}>`)
 
     // column for time
     let divTime = $("<div>").addClass("col-2").addClass("my-auto")
@@ -38,6 +42,9 @@ schedule.forEach(function(element){
     // Append the saveButton to divSave
     divSave.append(saveButton)
 
+    // format the divText colour according to the time
+    formatColour(divText, hour)
+
     // append div* elements to the row
     row.append(divTime)
     row.append(divText)
@@ -45,5 +52,25 @@ schedule.forEach(function(element){
     
     // Append the row to scheduleEl (".container")
     scheduleEl.append(row)
-    console.log(divTime)
+    
+    
 })
+
+
+function formatColour(divText, hour){
+
+
+    console.log(divText)
+    // Get the current Hour
+    var thisHour = parseInt(currentDay.format("H"))
+
+    //console.log(`Hour: ${hour} thisHour: ${thisHour}`)
+
+    if (hour < thisHour) {
+        $(divText).find("textarea").addClass("task-expired")
+    } else if (hour == thisHour) {
+        $(divText).find("textarea").addClass("task-current")
+    } else {
+        $(divText).find("textarea").addClass("task-next")
+    }
+}
